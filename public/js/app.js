25bee0de052afbcfc58f06b0ad0795db7ad3fd09
sumercedrafa <57945332+sumercedrafa@users.noreply.github.com>
2020-09-29 12:36:53 +0200

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,40 @@
+console.log('Client side javascript file is loaded')
+
+
+
+const weatherForm = document.querySelector('form')
+
+const search = document.querySelector('input')
+
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+messageOne.textContent = ''
+messageTwo.textContent = ''
+
+weatherForm.addEventListener('submit', (e) => {
+    const location = search.value
+
+    e.preventDefault()
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+
+
+    fetch('http://localhost:3000/weather?address=' + encodeURI(location)).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+            console.log(data.error) 
+            messageOne.textContent = data.error
+            messageTwo.textContent = ''} else 
+            {
+                console.log(data.location)
+                console.log(data.forecast)
+                messageOne.textContent = data.location
+                messageTwo.textContent = data.forecast
+            }
+        })
+    })
+
+
+
+})
---
+++ src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require('postman-request');
+
+const forecast = (latitude, longitude, callback) => {
+    const url = 'http://api.weatherstack.com/current?access_key=18b2ebabb41242de9b5353f8b78aa6b4&query=' + longitude + ',' + latitude
+
+    request({ url, json: true }, (error, { body } = {}) => {
+        if (error) {
+            callback('Unable to connect to weather service!', undefined)
+        } else if (body.error) {
+            callback('Unable to find location. Try another search.', undefined)
+        } else {
+            callback(undefined,  body.current.weather_descriptions[0]+'. Right now, the temperature is ' + body.current.temperature + ' Degrees Celsius. It feels like ' + body.current.feelslike + ' degrees.')
+            }
+    })
+}
+
+
+module.exports = forecast
